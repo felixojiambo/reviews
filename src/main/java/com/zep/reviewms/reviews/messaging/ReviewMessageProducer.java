5c3f975825cc453d09250be742449b3d4809ed26
Felix Ojiambo <felixojiamboe@gmail.com>
2024-04-01 07:52:04 -0700

    set producer as reviews

---
+++ src/main/java/com/zep/reviewms/reviews/messaging/ReviewMessageProducer.java
@@ -0,0 +1,22 @@
+package com.zep.reviewms.reviews.messaging;
+
+import com.zep.reviewms.reviews.Reviews;
+import com.zep.reviewms.reviews.dtos.ReviewMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+public class ReviewMessageProducer {
+    private  final RabbitTemplate rabbitTemplate;
+
+    public ReviewMessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    public  void  sendMessage(Reviews reviews){
+        ReviewMessage reviewMessage=new ReviewMessage();
+        reviewMessage.setId(reviewMessage.getId());
+        reviewMessage.setTitle(reviewMessage.getTitle());
+        reviewMessage.setDescription(reviewMessage.getDescription());
+        reviewMessage.setRating(reviewMessage.getRating());
+        reviewMessage.setCompanyId(reviews.getCompanyId());
+        rabbitTemplate.convertAndSend("companyRatingQueue",reviewMessage);
+    }
+}
